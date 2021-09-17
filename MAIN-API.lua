1e426396a266a5f92aa274b4156898bea15719ce
OriChanEzScript <90840960+OriChanEzScript@users.noreply.github.com>
2021-09-17 14:34:45 +0700

    Update MAIN-API.lua

--- MAIN-API.lua
+++ MAIN-API.lua
@@ -10,6 +10,9 @@ local StartInjecting = Instance.new("BindableFunction")
 local Player = game.Players.LocalPlayer
 local PlayerGui = Player:FindFirstChild("PlayerGui")
 
+local NoClippedParts = {}
+local IsNoClip = false
+
 local function ViewInventory(Player)
 	if game.Players:FindFirstChild(Player) then
 		local Player = game.Players:FindFirstChild(Player)
@@ -45,6 +48,96 @@ local function ViewInventory(Player)
 			Duration = 5,
 		})
 	end
+end
+
+local function ToggleFly()
+	-- Press E to toggle
+
+	repeat wait()
+	until Player and game.Players.LocalPlayer.Character and (Player.Character:FindFirstChild("UpperTorso") or Player.Character:FindFirstChild("Torso")) and Player.Character:findFirstChild("Humanoid")
+	local mouse = game.Players.LocalPlayer:GetMouse()
+	repeat wait() until mouse
+	local plr = game.Players.LocalPlayer
+	local UpperTorso = plr.Character:FindFirstChild("UpperTorso") or plr.Character:FindFirstChild("Torso") 
+	local flying = true
+	local deb = true
+	local ctrl = {f = 0, b = 0, l = 0, r = 0}
+	local lastctrl = {f = 0, b = 0, l = 0, r = 0}
+	local maxspeed = 50
+	local speed = 0
+
+	function Fly()
+		local bg = Instance.new("BodyGyro", UpperTorso)
+		bg.P = 9e4
+		bg.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+		bg.cframe = UpperTorso.CFrame
+		local bv = Instance.new("BodyVelocity", UpperTorso)
+		bv.velocity = Vector3.new(0,0.1,0)
+		bv.maxForce = Vector3.new(9e9, 9e9, 9e9)
+		repeat wait()
+			plr.Character.Humanoid.PlatformStand = true
+			if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then
+				speed = speed+.5+(speed/maxspeed)
+				if speed > maxspeed then
+					speed = maxspeed
+				end
+			elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then
+				speed = speed-1
+				if speed < 0 then
+					speed = 0
+				end
+			end
+			if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then
+				bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
+				lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r}
+			elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then
+				bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
+			else
+				bv.velocity = Vector3.new(0,0.1,0)
+			end
+			bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0)
+		until not flying
+		ctrl = {f = 0, b = 0, l = 0, r = 0}
+		lastctrl = {f = 0, b = 0, l = 0, r = 0}
+		speed = 0
+		bg:Destroy()
+		bv:Destroy()
+		plr.Character.Humanoid.PlatformStand = false
+	end
+	
+	local onKeyDown
+	onKeyDown = mouse.KeyDown:connect(function(key)
+		if key:lower() == "l" then
+			if flying then flying = false onKeyDown:Disconnect()
+			end
+		elseif key:lower() == "w" then
+			ctrl.f = 1
+		elseif key:lower() == "s" then
+			ctrl.b = -1
+		elseif key:lower() == "a" then
+			ctrl.l = -1
+		elseif key:lower() == "d" then
+			ctrl.r = 1
+		end
+	end)
+	
+	local onKeyUp
+	onKeyUp = mouse.KeyUp:connect(function(key)
+		if flying == true then
+			if key:lower() == "w" then
+				ctrl.f = 0
+			elseif key:lower() == "s" then
+				ctrl.b = 0
+			elseif key:lower() == "a" then
+				ctrl.l = 0
+			elseif key:lower() == "d" then
+				ctrl.r = 0
+			end
+		else
+			onKeyUp:Disconnect()
+		end
+	end)
+	Fly()
 end
 
 local function ExecuteString(String)
@@ -92,6 +185,51 @@ local function ExecuteString(String)
 		Target = string.gsub(Target, " ", "")
 		
 		ViewInventory(Target)
+	elseif string.sub(String, 0, 3) == "fly" then
+		spawn(function()
+			ToggleFly()
+		end)
+		
+		StarterGui:SetCore("SendNotification", {
+			Title = "PolaBeax - Fly",
+			Text = "Fly Enabled! Click [L] to unfly!",
+			Duration = 5,
+		})
+	elseif string.sub(String, 0, 6) == "noclip" then
+		if IsNoClip == false then
+			for _, Part in pairs(workspace:GetDescendants()) do
+				local ColSuc, Die = pcall(function()
+					if Part.CanCollide == true then
+						table.insert(NoClippedParts, Part)
+						Part.CanCollide = false
+					end
+				end)
+			end
+			
+			StarterGui:SetCore("SendNotification", {
+				Title = "PolaBeax - NoClip",
+				Text = "NoClip enabled! Use the command again to disable!",
+				Duration = 5,
+			})
+			
+			IsNoClip = true
+		else
+			for _, CollidedPart in pairs(NoClippedParts) do
+				if CollidedPart then
+					local ColSuc, Die = pcall(function()
+						CollidedPart.CanCollide = true
+					end)
+				end
+			end
+			
+			StarterGui:SetCore("SendNotification", {
+				Title = "PolaBeax - NoClip",
+				Text = "NoClip disabled! Use the command again to enable!",
+				Duration = 5,
+			})
+			
+			IsNoClip = false
+		end
 	end
 end
 
